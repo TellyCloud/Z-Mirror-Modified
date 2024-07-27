12b259362580fcb1e9234f6d66f2286b677c6105
TellyCloud <156745846+TellyCloud@users.noreply.github.com>
2024-07-27 17:11:37 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -242,7 +242,7 @@ async def get_readable_message(
             and int(config_dict["AUTO_DELETE_MESSAGE_DURATION"]) > 0
         ):
             msg += (
-                f"```#TellY{index + start_position}: "
+                f"```\n#TellY{index + start_position}: "
                 f"{escape(f"{task.name()}")}\n```"
                 if elapse <= config_dict["AUTO_DELETE_MESSAGE_DURATION"]
                 else f"<blockquote>\n#TellY{index + start_position}...(Processing)</blockquote>"
