97040ba2f58556b0412e865e81dcd1b767345503
TellyCloud <156745846+TellyCloud@users.noreply.github.com>
2024-08-08 10:32:36 +0530

    Update token_manager.py

--- bot/helper/ext_utils/token_manager.py
+++ bot/helper/ext_utils/token_manager.py
@@ -14,7 +14,7 @@ from bot.helper.ext_utils.shortener import short_url
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.button_build import ButtonMaker
 from bot.helper.telegram_helper.filters import CustomFilters
-from bot.helper.telegram_helper.message_utils import sendMessage
+from bot.helper.telegram_helper.message_utils import sendMessage, sendLogMessage
 
 from pyrogram.filters import command
 from pyrogram.handlers import MessageHandler
@@ -22,8 +22,14 @@ from pyrogram.handlers import MessageHandler
 
 async def checking_access(user_id, button=None):
     if not config_dict["TOKEN_TIMEOUT"]:
-        return None, button
-    user_data.setdefault(user_id, {})
+        return (
+            None,
+            button
+        )
+    user_data.setdefault(
+        user_id,
+        {}
+    )
     data = user_data[user_id]
     if DATABASE_URL:
         data["time"] = await DbManager().get_token_expire_time(user_id)
@@ -40,29 +46,44 @@ async def checking_access(user_id, button=None):
             and "token" in data
             else str(uuid4())
         )
+        inittime = time()
         if expire is not None:
             del data["time"]
         data["token"] = token
+        data["inittime"] = inittime
         if DATABASE_URL:
             await DbManager().update_user_token(
                 user_id,
-                token
+                token,
+                inittime
             )
         user_data[user_id].update(data)
         if button is None:
             button = ButtonMaker()
         button.ubutton(
             "Get New Token",
-            short_url(f"https://telegram.me/{bot_name}?start={token}")
+            short_url(f"https://redirect.z-mirror.eu.org/{bot_name}/{token}")
         )
-        tmsg = "Your <b>Token</b> is expired. Get a new one."
-        tmsg += f"\n<b>Token Validity</b>: {get_readable_time(config_dict["TOKEN_TIMEOUT"])}"
-        return tmsg, button
-    return None, button
+        tmsg = (
+            "Your <b>Token</b> is expired. Get a new one."
+            f"\n<b>Token Validity</b>: {get_readable_time(config_dict["TOKEN_TIMEOUT"])}\n"
+        )
+        return (
+            tmsg,
+            button
+        )
+    return (
+        None,
+        button
+    )
 
 
 async def start(client, message):
-    if len(message.command) > 1 and len(message.command[1]) == 36:
+    tag = message.from_user.mention
+    if (
+        len(message.command) > 1
+        and len(message.command[1]) == 36
+    ):
         userid = message.from_user.id
         input_token = message.command[1]
         if DATABASE_URL:
@@ -76,6 +97,28 @@ async def start(client, message):
                 return await sendMessage(
                     message,
                     "Invalid token.\n\nPlease generate a new one."
+                )
+            inittime = await DbManager().get_token_init_time(userid)
+            duration = time() - inittime # type: ignore
+            if (
+                config_dict["MINIMUM_DURATOIN"]
+                and (
+                    duration < config_dict["MINIMUM_DURATOIN"]
+                )
+            ):
+                await DbManager().update_user_tdata(
+                    userid,
+                    0,
+                    0
+                )
+                await sendLogMessage(
+                    message,
+                    f"#BYPASS\n\nShortener bypass detected.",
+                    tag
+                )
+                return await sendMessage(
+                    message,
+                    "Shortener bypass detected.\n\nPlease generate a new token.\n\n<b>Don't try to bypass it, next time ban.</b>"
                 )
         if userid not in user_data:
             return await sendMessage(
@@ -91,6 +134,23 @@ async def start(client, message):
                 message,
                 "Token already used!\n\nKindly generate a new one."
             )
+        duration = time() - data["inittime"]
+        if (
+            config_dict["MINIMUM_DURATOIN"]
+            and (
+                duration < config_dict["MINIMUM_DURATOIN"]
+            )
+        ):
+            del data["token"]
+            await sendLogMessage(
+                message,
+                f"#BYPASS\n\nShortener bypass detected.",
+                tag
+            )
+            return await sendMessage(
+                message,
+                "Shortener bypass detected.\n\nPlease generate a new token.\n\n<b>Don't try to bypass it, next time ban.</b>"
+            )
         token = str(uuid4())
         ttime = time()
         data["token"] = token
@@ -102,8 +162,12 @@ async def start(client, message):
                 token,
                 ttime
             )
-        msg = "Token refreshed successfully!\n\n"
-        msg += f"Validity: {get_readable_time(int(config_dict["TOKEN_TIMEOUT"]))}"
+        msg = (
+            "Your token refreshed successfully!\n"
+            f"Validity: {get_readable_time(int(config_dict["TOKEN_TIMEOUT"]))}\n\n"
+            "<b>Your Limites:</b>\n"
+            f"{config_dict["USER_MAX_TASKS"]} parallal tasks.\n"
+        )
         return await sendMessage(
             message,
             msg
@@ -118,20 +182,23 @@ async def start(client, message):
     elif (
         not config_dict["DM_MODE"]
         and message.chat.type != message.chat.type.SUPERGROUP
-        and not await CustomFilters.authorized(client, message)
+        and not await CustomFilters.authorized(
+            client,
+            message
+        )
     ):
-        start_string = "Sorry, you cannot use me here!\n" \
-                       "Join: @TellYcloud_Bots to use me.\n" \
-                       "Thank You!"
+        start_string = "Sorry, you cannot use me in private!"
     elif (
         not config_dict["DM_MODE"]
         and message.chat.type != message.chat.type.SUPERGROUP
-        and await CustomFilters.authorized(client, message)
+        and await CustomFilters.authorized(
+            client,
+            message
+        )
     ):
         start_string = "There's nothing to Start here.\n" \
                        "Try something else or read HELP"
     else:
-        tag = message.from_user.mention
         start_string = "Start me in DM, not in the group.\n" \
                        f"cc: {tag}"
     await sendMessage(
